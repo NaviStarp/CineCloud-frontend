a32c4761b5f23e1a5c643ac7bb9cd69364c2b00a
NaviStarp <ivan14pratsavila@gmail.com>
2025-05-11 15:11:15 +0200

    Deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+import { APP_BASE_HREF } from '@angular/common';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimations()]
+  providers: [{ provide: APP_BASE_HREF, useValue: '/CineCloud-frontend/' },provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimations()]
 };
