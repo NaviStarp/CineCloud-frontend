1b43a933d5a8cbe7015ba62f5a3d34ceb78b5a18
Ivan <ivan14pratsavila@gmail.com>
2025-05-12 11:06:49 +0200

    Cambio url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { APP_BASE_HREF } from '@angular/common';
 export const appConfig: ApplicationConfig = {
-  providers: [{ provide: APP_BASE_HREF, useValue: '/CineCloud-frontend/' },provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimations()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimations()]
 };
